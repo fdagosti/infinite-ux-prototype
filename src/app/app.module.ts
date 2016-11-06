e4357c973849f4e8fec31444adae9e2bade771ef
fdagosti <francois.dagostini@gmail.com>
2016-11-06 21:48:01 +0100

    added carousel and flex box for list items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
+import {RouterModule} from "@angular/router";
 import {PrimeTimeComponent} from "./prime-time/prime-time.component";
 import {IUXNavbarComponent} from "./iux-navbar/iux-navbar.component";
 import {IUXContentListLgComponent} from "./iux-content-list-lg/iux-content-list-lg.component";
 import {IUXFooterComponent} from "./iux-footer/iux-footer.component";
 import {IUXContentListSmComponent} from "./iux-content-list-sm/iux-content-list-sm.component";
-import { HomeComponent } from './home/home.component';
-import { PresentationComponent } from './presentation/presentation.component';
+import {HomeComponent} from "./home/home.component";
+import {PresentationComponent} from "./presentation/presentation.component";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,12 @@ import { PresentationComponent } from './presentation/presentation.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: "presentation", component: PresentationComponent},
+      {path: "", component: HomeComponent},
+    ]),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
