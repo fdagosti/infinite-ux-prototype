a7dab4e359156e2e838bcca886a8f1027338661e
fdagosti <francois.dagostini@gmail.com>
2016-11-23 15:26:52 +0100

    add high resolution images and vertical full content

--- src/app/dago/content-row/content-row.component.ts
+++ src/app/dago/content-row/content-row.component.ts
@@ -41,7 +41,7 @@ export class ContentRowComponent implements OnInit {
   getContentImage(index){
 
     if (this.content && this.content.content[index]){
-      return this.content.content[index].content.media[this.vertical?3:0].url;
+      return this.content.content[index].content.media[this.vertical?5:2].url;
     }else{
       return `http://placehold.it/${this.imageSize.w}x${this.imageSize.h}/000000/ffffff?text=+`;
     }
