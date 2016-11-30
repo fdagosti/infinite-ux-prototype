f96614078b201b68cba3f4e6564381d311fa1aab
Duchastenier, Peter <PDuchastenier@nds.com>
2016-11-30 14:54:48 +0100

    Profile Gate : new Component

---
+++ src/app/peter/profiles-gate/profiles-gate.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ProfilesGateComponent } from './profiles-gate.component';
+
+describe('Component: ProfilesGate', () => {
+  it('should create an instance', () => {
+    let component = new ProfilesGateComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/peter/profiles-gate/profiles-gate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iux-profiles-gate',
+  templateUrl: './profiles-gate.component.html',
+  styleUrls: ['./profiles-gate.component.css']
+})
+export class ProfilesGateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
