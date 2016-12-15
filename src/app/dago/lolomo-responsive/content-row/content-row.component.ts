42a8a34173f4d71a255860233cd054984e9af473
fdagosti <francois.dagostini@gmail.com>
2016-12-15 10:16:11 +0100

    first final version of responsive lolomo

--- src/app/dago/lolomo-responsive/content-row/content-row.component.ts
+++ src/app/dago/lolomo-responsive/content-row/content-row.component.ts
@@ -8,7 +8,7 @@ import {
   OnChanges,
   SimpleChanges,
   Output,
-  EventEmitter
+  EventEmitter, trigger, state, style, transition, animate, NgZone
 } from "@angular/core";
 
 
@@ -25,12 +25,64 @@ const LANDSCAPE_SIZE = {w:399,h:225};
   selector: 'iux-content-row',
   templateUrl: 'content-row.component.html',
   styleUrls: ['content-row.component.css'],
+  animations: [
+    trigger('horizontalScroll', [
+
+      state('next',style({})),
+      state('previous',   style({})),
+      state('stop',   style({})),
+      transition('* => next0', [
+        animate('750ms ease-out', style({transform: 'translate3d(-100%,0px, 0px)'}))
+      ]),
+      transition('* => next100', [
+        animate('750ms ease-out', style({transform: 'translate3d(-200%,0px, 0px)'}))
+      ]),
+      transition('* => next116', [
+        animate('750ms ease-out', style({transform: 'translate3d(-216.66666666%,0px, 0px)'}))
+      ]),
+      transition('* => next120', [
+        animate('750ms ease-out', style({transform: 'translate3d(-220%,0px, 0px)'}))
+      ]),
+      transition('* => next125', [
+        animate('750ms ease-out', style({transform: 'translate3d(-225%,0px, 0px)'}))
+      ]),
+      transition('* => next133', [
+        animate('750ms ease-out', style({transform: 'translate3d(-233.3333333%,0px, 0px)'}))
+      ]),
+      transition('* => next150', [
+        animate('750ms ease-out', style({transform: 'translate3d(-250%,0px, 0px)'}))
+      ]),
+      transition('* => previous0', [
+        animate('750ms ease-out', style({transform: 'translate3d(0%,0px, 0px)'}))
+      ]),
+      transition('* => previous100', [
+        animate('750ms ease-out', style({transform: 'translate3d(0%,0px, 0px)'}))
+      ]),
+      transition('* => previous116', [
+        animate('750ms ease-out', style({transform: 'translate3d(-16.66666666%,0px, 0px)'}))
+      ]),
+      transition('* => previous120', [
+        animate('750ms ease-out', style({transform: 'translate3d(-20%,0px, 0px)'}))
+      ]),
+      transition('* => previous125', [
+        animate('750ms ease-out', style({transform: 'translate3d(-25%,0px, 0px)'}))
+      ]),
+      transition('* => previous133', [
+        animate('750ms ease-out', style({transform: 'translate3d(-33.333333%,0px, 0px)'}))
+      ]),
+      transition('* => previous150', [
+        animate('750ms ease-out', style({transform: 'translate3d(-50%,0px, 0px)'}))
+      ]),
+    ])
+  ]
 
 })
 export class ContentRowComponent implements OnInit, AfterViewInit, OnChanges {
 
 
-  constructor() { }
+
+
+  constructor(private zone: NgZone) { }
 
   @Input() private fullContent;
   private fullContentOffset = 0;
@@ -100,8 +152,8 @@ export class ContentRowComponent implements OnInit, AfterViewInit, OnChanges {
 
     let actualPastItems = this.fullContentOffset - computedOffset;
     let oneItemPercentage = 100 / this.numberOfVisibleItems;
-    let percentageOffset = actualPastItems * oneItemPercentage;
-    this.slider.nativeElement.style.transform=`translate3d(-${percentageOffset}%,0px, 0px)`;
+    this.percentageOffset = actualPastItems * oneItemPercentage;
+    this.slider.nativeElement.style.transform=`translate3d(-${this.percentageOffset}%,0px, 0px)`;
   }
 
   private getImageSize(portrait){
@@ -112,20 +164,64 @@ export class ContentRowComponent implements OnInit, AfterViewInit, OnChanges {
     }
   }
 
+  animationStarted(e){
+    // console.log("ANIMATION Started ",e);
+  }
+
+  animationDone(e){
+    // console.log("Animation Done ",e);
+
+    // Bug workaround. See https://github.com/angular/angular/issues/11881
+    this.zone.run(() => {
+      this.pageAnimState="stop";
+      this.fillWindow();
+      this.inAnim = false;
+    });
+
+  }
+
+  pageAnimState;
+  private percentageOffset;
+  private nextAnim = {
+    "0":"next0",
+    "100":"next100",
+    "116":"next116",
+    "120":"next120",
+    "125":"next125",
+    "133":"next133",
+    "150":"next150",
+  }
+  private prevAnim = {
+    "0":"previous0",
+    "100":"previous100",
+    "116":"previous116",
+    "120":"previous120",
+    "125":"previous125",
+    "133":"previous133",
+    "150":"previous150",
+  }
+  private inAnim = false;
+
   next(){
+    if (this.inAnim) return;
+    // console.log("NEXT ",this.pageAnimState, this.percentageOffset, this.nextAnim[Math.floor(this.percentageOffset)]);
+    this.inAnim = true;
+    this.pageAnimState = this.nextAnim[Math.floor(this.percentageOffset)];
     this.fullContentOffset=(this.fullContentOffset + this.numberOfVisibleItems);
     if (this.fullContentOffset >= this.fullContent.length) this.fullContentOffset = 0;
     this.fullContentOffsetEmitter.emit(this.fullContentOffset);
-    this.fillWindow();
+
 
   }
 
   prev(){
+    if (this.inAnim) return;
+    // console.log("PREV ",this.pageAnimState, this.percentageOffset, this.prevAnim[Math.floor(this.percentageOffset)]);
+    this.inAnim = true;
+    this.pageAnimState = this.prevAnim[Math.floor(this.percentageOffset)];
     this.fullContentOffset-=this.numberOfVisibleItems;
     this.fullContentOffset = Math.max(this.fullContentOffset, 0);
     this.fullContentOffsetEmitter.emit(this.fullContentOffset);
-    this.fillWindow();
-    // this.slider.nativeElement.style.transform='translate3d(100%,0px, 0px)';
   }
 
   private getPlayLink(program){
