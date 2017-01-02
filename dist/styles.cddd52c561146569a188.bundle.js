9e2a380431d59f07b1977fcd07b076686f7a96e0
fdagosti <francois.dagostini@gmail.com>
2017-01-02 21:56:04 +0100

    new build

--- dist/styles.cddd52c561146569a188.bundle.js
+++ dist/styles.cddd52c561146569a188.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([4,5],{1141:function(n,o,c){c(515),c(516),c(514),c(517),c(518),n.exports=c(519)},514:function(n,o){},515:function(n,o){},516:function(n,o){},517:function(n,o){},518:function(n,o){},519:function(n,o){}},[1141]);
+webpackJsonp([4,5],{1142:function(n,o,c){c(516),c(517),c(515),c(518),c(519),n.exports=c(520)},515:function(n,o){},516:function(n,o){},517:function(n,o){},518:function(n,o){},519:function(n,o){},520:function(n,o){}},[1142]);
 //# sourceMappingURL=styles.cddd52c561146569a188.bundle.map
