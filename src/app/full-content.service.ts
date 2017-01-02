ea1c030eecbcd730c69bff743d6960b7f2e0a8c5
fdagosti <francois.dagostini@gmail.com>
2017-01-02 21:25:40 +0100

    create Full content service to remove twitch dependencies from core graphic components

--- src/app/full-content.service.ts
+++ src/app/full-content.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
+import {Params} from "@angular/router";
+import {Observable} from "rxjs";
+import {IVPService} from "./ivp.service";
+import {TwitchService} from "./twitch.service";
 
 @Injectable()
 export class FullContentService {
 
-  constructor() { }
+  constructor(private ctap: IVPService,private twitch: TwitchService) { }
+
+  public getFullContentData(params:Params, offset){
+    if (params["contentName"]){
+      return this.ctap.getContentFromSearchedTerm(params['contentName'], 0, "200")
+        .map(result=>({
+          id: params['contentName'],
+          title: params['contentName'],
+          content: result,
+          portrait: false
+        }));
+    }else if (params['categoryId'] && params['categoryId']=="live"){
+      return this.ctap.getChannels( 0, "200")
+        .map(result=>({
+          id: params["categoryId"],
+          title: "Live",
+          content: result,
+          portrait: false
+        }));
+
+    }else if (params['categoryId'] && params['categoryId']=="twitch"){
+      return this.twitch.getTopGames(offset, "30")
+        .map(result=>({
+          id: params["categoryId"],
+          title: "Twitch Top Games",
+          content: result,
+          portrait: true
+        }))
+    }else if (params['categoryId']) {
+      return Observable.forkJoin(
+        this.ctap.getContent(params['categoryId'], offset, "30"),
+        this.ctap.getCategories(params['categoryId']))
+        .take(1)
+        .map(a=>({
+          id: params["categoryId"],
+          title: a[1].name,
+          content: a[0],
+          portrait: true
+        }))
+    }
+    return null;
+  }
+
+  // TODO to merge with the previous method when needed
+  public getScrollUpdateData(id, offset){
+  if (id === "twitch"){
+    return this.twitch.getTopGames(offset, "30");
+  }else{
+    return this.ctap.getContent(id, offset, "30");
+  }
+}
+
 
 }
--- src/app/infinite-ux/full-content/full-content.component.ts
+++ src/app/infinite-ux/full-content/full-content.component.ts
@@ -1,10 +1,8 @@
-import {Component, OnInit, Input} from "@angular/core";
-import {IVPService, Category} from "../../ivp.service";
+import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
 import "rxjs/add/operator/switchMap";
-import {Observable} from "rxjs";
 import {JawboneService} from "../jawbone.service";
-import {TwitchService} from "../../twitch.service";
+import {FullContentService} from "../../full-content.service";
 
 
 @Component({
@@ -17,12 +15,18 @@ import {TwitchService} from "../../twitch.service";
 })
 export class FullContentComponent implements OnInit {
 
-  private content;
   private pageNum=0;
   private pages=Array(0);
-  private categoryId;
-  private title:string;
-  private catBased = true;
+
+  private fullContentData = {
+    id : "toto",
+    portrait : true,
+    title: "",
+    content:{
+      content: [],
+      total: 0
+    }
+  };
 
   private numberOfItemsPerPage = 2;
   private projectedContent=[];
@@ -31,8 +35,7 @@ export class FullContentComponent implements OnInit {
   private jawboneOpened=[];
 
   constructor(
-    private ctap:IVPService,
-    private twitch:TwitchService,
+    private fullContentService:FullContentService,
     private route:ActivatedRoute,
     private jawbone:JawboneService
   ) {}
@@ -41,102 +44,50 @@ export class FullContentComponent implements OnInit {
     this.inData = true;
     this.route.params
       .switchMap((params:Params) => this.getObservable(params))
+      .do(()=>this.inData = false)
       .subscribe(
-        () => {this.inData = false;this.computePageSize()},
+        (v) => {this.fullContentData = v;this.inData = false;this.computePageSize()},
         error => console.log("ERRROR",error),
         ()=>console.log("REQUEST ENDED")
       );
   }
 
   private getObservable(params:Params){
-    if (params["contentName"]){
-      return this.ctap.getContentFromSearchedTerm(params['contentName'], 0, "200")
-        .do(result => {
-          this.categoryId = params['contentName'];
-          this.content = result;
-          this.title = params['contentName'];
-          this.catBased= false;
-        })
-    }else if (params['categoryId'] && params['categoryId']=="live"){
-      return this.ctap.getChannels( 0, "200")
-        .do(result => {
-          this.categoryId = params['categoryId'];
-          this.content = result;
-          this.title = "Live";
-          this.catBased= false;
-        });
-    }else if (params['categoryId'] && params['categoryId']=="twitch"){
-      return this.twitch.getTopGames(this.currentOffset, "30")
-        .do(result => {
-          console.log("result twitch ",result);
-          this.categoryId = params['categoryId'];
-          this.content = result;
-          this.title = "Twitch Top Games";
-          this.catBased = true;
-        });
-    }else if (params['categoryId']) {
-      return Observable.forkJoin(
-        this.ctap.getContent(params['categoryId'], this.currentOffset, "30"),
-        this.ctap.getCategories(params['categoryId']))
-        .take(1)
-        .map(a=>({
-          id: params["categoryId"],
-          title: a[1].name,
-          content: a[0],
-          portrait: true
-        }))
-        .do(r=>console.log("r",r))
-        .do(result => {
-          this.categoryId = result.id;
-          this.content = result.content;
-          this.title = result.title;
-          this.catBased= result.portrait;
-        })
-    }
+    return this.fullContentService.getFullContentData(params, this.currentOffset);
   }
 
   private currentOffset = 0;
 
   private inData = false;
 
-  private _getObservable(cat){
-    if (cat === "twitch"){
-      return this.twitch.getTopGames(this.currentOffset, "30");
-    }else{
-      return this.ctap.getContent(this.categoryId, this.currentOffset, "30");
-    }
-  }
-
   private fetchCategoryContent(){
     if (this.inData) return;
     this.currentOffset += 30;
     this.inData = true;
-    this.busy = this._getObservable(this.categoryId).subscribe(
-      v=>{this.content.content = this.content.content.concat(v.content)},
+    this.busy = this.fullContentService.getScrollUpdateData(this.fullContentData.id, this.currentOffset).subscribe(
+      v=>{this.fullContentData.content.content = this.fullContentData.content.content.concat(v.content)},
       e=>console.log("error",e),
       ()=>{this.inData = false;this.computePageSize()}
     );
   }
 
   private isCacheFull(){
-    return this.content?this.content.content.length >= this.content.total:false;
+    return this.fullContentData.content?this.fullContentData.content.content.length >= this.fullContentData.content.total:false;
   }
 
   onScroll(){
-    if (this.catBased && !this.isCacheFull())
+    if (this.fullContentData.portrait && !this.isCacheFull())
       this.fetchCategoryContent();
   }
 
   jawboneChanged(ev,i){
     this.jawboneOpened.fill(false);
     this.jawboneOpened[i] = ev;
-    console.log("jawbone changed ",ev,i);
   }
 
   closeJawbone(i){
     this.jawboneOpened[i]=false
     this.jawbone.setJawboneStatus(i, false);
-    console.log("close jawbone ",i);
   }
 
   updateVisibleItems(ev, idx){
@@ -150,12 +101,12 @@ export class FullContentComponent implements OnInit {
   }
 
   computePageSize(){
-    this.pageNum = Math.ceil(this.content.content.length/this.numberOfItemsPerPage);
+    this.pageNum = Math.ceil(this.fullContentData.content.content.length/this.numberOfItemsPerPage);
     let temporaryArray = Array(this.pageNum).fill(1);
     this.pages = temporaryArray.map((x, i) => i*this.numberOfItemsPerPage);
     this.jawbone.setNumberOfRows(this.pageNum);
     this.closeAllJawbones();
-    this.projectedContent = this.pages.map((v)=>this.content.content.slice(v, v+this.numberOfItemsPerPage))
+    this.projectedContent = this.pages.map((v)=>this.fullContentData.content.content.slice(v, v+this.numberOfItemsPerPage))
   }
 
   closeAllJawbones(){
